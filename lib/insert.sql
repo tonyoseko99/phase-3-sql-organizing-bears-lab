35897d19bc651a0f1dddbe4ee4eb98fa1db173a0
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-25 10:57:49 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES 
+    ('Mr. Chocolate', 10, 'M', 'Red', 'Melancholic', true),
+    ('Rowdy', 10, 'M', 'Red', 'Sanguine', false),
+    ('Tabitha', 10, 'F', 'Blue', 'Sanguine', true),
+    ('Sergeant Brown', 10, 'M', 'Red', 'Sanguine', true),
+    ('Melissa', 10, 'F', 'Orange', 'Sanguine', false),
+    ('Grinch', 10, 'M', 'Green', 'Sanguine', false),
+    ('Wendy', 10, 'F', 'Maroon', 'Sanguine', true),
+    (NULL, 10, 'M', 'Pink', 'Sanguine', false);
